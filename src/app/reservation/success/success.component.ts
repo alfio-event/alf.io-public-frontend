8be1963eaee4cd972fdd9acaca08ca882ef2a342
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-04-09 16:10:55 +0200

    #1 initial work on success page

--- src/app/reservation/success/success.component.ts
+++ src/app/reservation/success/success.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ReservationService } from '../../shared/reservation.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Event } from 'src/app/model/event';
+import { EventService } from 'src/app/shared/event.service';
 
 @Component({
   selector: 'app-success',
@@ -10,15 +12,26 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SuccessComponent implements OnInit {
 
   success: any;
+  eventShortName: string;
+  reservationId: string;
+
+  event: Event;
+
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private reservationService: ReservationService) { }
+    private reservationService: ReservationService,
+    private eventService: EventService) { }
 
   ngOnInit() {
     this.route.parent.params.subscribe(params => {
-      this.reservationService.getSuccess(params['eventShortName'], params['reservationId']).subscribe(res => {
+      this.eventShortName = params['eventShortName'];
+      this.reservationId = params['reservationId'];
+      this.eventService.getEvent(this.eventShortName).subscribe(ev => {
+        this.event = ev;
+      });
+      this.reservationService.getSuccess(this.eventShortName, this.reservationId).subscribe(res => {
         this.success = res;
       })
     });
