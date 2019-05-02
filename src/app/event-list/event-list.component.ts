96188fbd1c0264b196440500803815909aa42388
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-05-02 14:14:55 +0200

    #1 load languages

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
 import { Router } from '@angular/router';
 import { BasicEventInfo } from '../model/basic-event-info';
+import { I18nService } from '../shared/i18n.service';
+import { ContentLanguage } from '../model/event';
 
 @Component({
   selector: 'app-event-list',
@@ -10,8 +12,12 @@ import { BasicEventInfo } from '../model/basic-event-info';
 export class EventListComponent implements OnInit {
 
   events: BasicEventInfo[];
+  languages: ContentLanguage[];
 
-  constructor(private eventService: EventService, private router: Router) { }
+  constructor(
+    private eventService: EventService, 
+    private i18nService: I18nService,
+    private router: Router) { }
 
   ngOnInit() {
     this.eventService.getEvents().subscribe(res => {
@@ -20,6 +26,10 @@ export class EventListComponent implements OnInit {
       } else {
         this.events = res;
       }
-    })
+    });
+
+    this.i18nService.getAvailableLanguages().subscribe(res => {
+      this.languages = res;
+    });
   }
 }
