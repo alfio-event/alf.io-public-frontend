cec16cac0d4464e2792c1359dde7f7cf070f7452
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-05-08 11:12:26 +0200

    #1 update language rest api

--- src/app/shared/i18n.service.ts
+++ src/app/shared/i18n.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
-import { ContentLanguage } from '../model/event';
+import { Language } from '../model/event';
 import { LocalizedCountry } from '../model/localized-country';
 
 @Injectable({
@@ -15,7 +15,7 @@ export class I18nService {
       return this.http.get<LocalizedCountry[]>(`/api/v2/public/i18n/countries/${locale}`);
   }
 
-  getAvailableLanguages(): Observable<ContentLanguage[]> {
-    return this.http.get<ContentLanguage[]>(`/api/v2/public/i18n/languages`);
+  getAvailableLanguages(): Observable<Language[]> {
+    return this.http.get<Language[]>(`/api/v2/public/i18n/languages`);
   }
 }
