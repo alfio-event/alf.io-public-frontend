c1841e2d7d14324a078a0ed50961a2acd0b78146
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-05-23 17:00:23 +0200

    additional service: more work #1

--- src/app/additional-service/additional-service.component.ts
+++ src/app/additional-service/additional-service.component.ts
@@ -1,16 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { AdditionalService } from '../model/additional-service';
 import { TranslateService } from '@ngx-translate/core';
+import { FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-additional-service',
   templateUrl: './additional-service.component.html'
 })
-export class AdditionalServiceComponent {
+export class AdditionalServiceComponent implements OnInit, OnDestroy {
 
   @Input()
   additionalService: AdditionalService;
 
+  @Input()
+  form: FormGroup;
+
+  private formSub: Subscription;
+
   constructor(public translate: TranslateService) { }
+
+  public ngOnInit(): void {
+    
+    //we only need to recalculate the select box choice in this specific supplement policy!
+    if (this.additionalService.supplementPolicy === 'OPTIONAL_MAX_AMOUNT_PER_TICKET') {
+      this.formSub = this.form.get('reservation').valueChanges.subscribe(valueChange => {
+        console.log('value changed!', valueChange);
+      });
+    }
+  }
+
+  public ngOnDestroy(): void {
+    if (this.formSub) {
+      this.formSub.unsubscribe();
+    }
+  }
 
 }
