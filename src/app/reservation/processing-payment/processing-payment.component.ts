105c6a7826d4458af65d5b4dbf0cdb8eb62852af
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-05-31 18:15:40 +0200

    use set interval to check reservation status #1

--- src/app/reservation/processing-payment/processing-payment.component.ts
+++ src/app/reservation/processing-payment/processing-payment.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ReservationInfo } from 'src/app/model/reservation-info';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ReservationService } from 'src/app/shared/reservation.service';
 import { EventService } from 'src/app/shared/event.service';
 import { zip } from 'rxjs';
@@ -11,7 +11,7 @@ import { I18nService } from 'src/app/shared/i18n.service';
   selector: 'app-processing-payment',
   templateUrl: './processing-payment.component.html'
 })
-export class ProcessingPaymentComponent implements OnInit {
+export class ProcessingPaymentComponent implements OnInit, OnDestroy {
 
   reservationInfo: ReservationInfo;
   event: Event;
@@ -19,8 +19,11 @@ export class ProcessingPaymentComponent implements OnInit {
   eventShortName: string;
   reservationId: string;
 
+  private intervalId: any;
+
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private reservationService: ReservationService,
     private eventService: EventService,
     private i18nService: I18nService
@@ -36,7 +39,21 @@ export class ProcessingPaymentComponent implements OnInit {
         this.reservationInfo = reservationInfo;
         this.i18nService.setPageTitle('show-ticket.header.title', ev.displayName);
       })
+
+      this.intervalId = setInterval(() => {
+        this.reservationService.getReservationStatusInfo(this.eventShortName, this.reservationId).subscribe(res => {
+          if (res.status === 'COMPLETE') {
+            clearInterval(this.intervalId);
+            this.router.navigate(['event', this.eventShortName, 'reservation', this.reservationId, 'success'])
+          }
+        });
+      }, 2000);
     });
   }
+
+  public ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
 
 }
