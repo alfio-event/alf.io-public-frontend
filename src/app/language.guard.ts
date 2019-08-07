f2a1d8e24baeb26afc6ab969c62f450ad2ec8e1e
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-08-07 15:13:59 +0200

    #18 support lang query string override

--- src/app/language.guard.ts
+++ src/app/language.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable, of, zip } from 'rxjs';
-import { I18nService, CustomLoader } from './shared/i18n.service';
+import { Observable } from 'rxjs';
+import { I18nService } from './shared/i18n.service';
 import { EventService } from './shared/event.service';
 import { TranslateService } from '@ngx-translate/core';
-import { map, mergeMap, switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,12 @@ export class LanguageGuard implements CanActivate {
 
   //
 
-  constructor(private i18nService: I18nService, private eventService: EventService, private translate: TranslateService, private customLoader: CustomLoader) {
+  constructor(private i18nService: I18nService, private eventService: EventService, private translate: TranslateService) {
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
 
+    const langQueryParam = next.queryParams['lang'];
     const persisted = this.i18nService.getPersistedLanguage();
 
     //set before calling, to avoid any strange flashes
@@ -30,7 +31,7 @@ export class LanguageGuard implements CanActivate {
     const req = eventShortName ? this.getForEvent(eventShortName) : this.getForApp();
 
     return req.pipe(switchMap(availableLanguages => {
-      const lang = this.extractLang(availableLanguages, persisted);
+      const lang = this.extractLang(availableLanguages, persisted, langQueryParam);
       return this.i18nService.useTranslation(eventShortName, lang);
     }))
   }
@@ -43,9 +44,11 @@ export class LanguageGuard implements CanActivate {
     return this.i18nService.getAvailableLanguages().pipe(map(languages => languages.map(l => l.locale)));
   }
 
-  private extractLang(availableLanguages: string[], persisted: string): string {
+  private extractLang(availableLanguages: string[], persisted: string, override: string): string {
     var lang;
-    if (availableLanguages.indexOf(persisted) >= 0) {
+    if (override && availableLanguages.indexOf(override) >= 0) {
+      lang = override;
+    } else if (availableLanguages.indexOf(persisted) >= 0) {
       lang = persisted;
     } else if (availableLanguages.indexOf(this.translate.getBrowserLang()) >= 0) {
       lang = this.translate.getBrowserLang();
