3946d4072373f093f5161995c523fb8e526c5dab
Celestino Bellone <celestino@swicket.io>
2019-10-11 14:24:33 +0200

    fix #25 - redesign language selector

--- src/app/language-selector/language-selector.component.ts
+++ src/app/language-selector/language-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Language } from '../model/event';
 import { Router } from '@angular/router';
 import { I18nService } from '../shared/i18n.service';
@@ -8,15 +8,32 @@ import { I18nService } from '../shared/i18n.service';
   templateUrl: './language-selector.component.html',
   styleUrls: ['./language-selector.component.scss']
 })
-export class LanguageSelectorComponent {
+export class LanguageSelectorComponent implements OnInit {
 
   @Input()
-  contentLanguages: Language[];
+  private contentLanguages: Language[];
+  private selectedLanguage: string;
+  currentLanguage: string;
+  filteredLanguages: Language[];
 
   constructor(private i18nService: I18nService, private router: Router) { }
 
+  ngOnInit(): void {
+    this.selectedLanguage = this.i18nService.getCurrentLang();
+    this.buildValues();
+  }
+
+  private buildValues(): void {
+    const currentLang = this.contentLanguages.find(cl => cl.locale === this.selectedLanguage);
+    this.currentLanguage = currentLang ? currentLang.displayLanguage : null;
+    this.filteredLanguages = this.contentLanguages.filter(cl => cl !== currentLang).sort((a, b) => a.displayLanguage.toLowerCase() > b.displayLanguage.toLowerCase() ? 1 : -1);
+  }
+
   public changeLanguage(lang: string): void {
     const eventShortName = this.router.routerState.snapshot.root.firstChild ? this.router.routerState.snapshot.root.firstChild.params['eventShortName'] : null;
-    this.i18nService.useTranslation(eventShortName, lang).subscribe(r => {});
+    this.i18nService.useTranslation(eventShortName, lang).subscribe(() => {
+      this.selectedLanguage = this.i18nService.getCurrentLang();
+      this.buildValues();
+    });
   }
 }
