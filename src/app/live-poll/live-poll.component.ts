92d646da4eb359ff7248241b1674b51709d6ed98
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-09-19 15:01:13 +0200

    initial stubs for service

--- src/app/live-poll/live-poll.component.ts
+++ src/app/live-poll/live-poll.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import { EventService } from '../shared/event.service';
 
 @Component({
   selector: 'app-live-poll',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivePollComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private eventService: EventService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      let eventShortName = params['eventShortName'];
+      this.eventService.getEvent(eventShortName).subscribe(ev => {
+        //this.i18nService.setPageTitle('reservation-page-not-found.header.title', ev.displayName);
+        console.log(ev);
+      });
+    });
   }
 
 }
