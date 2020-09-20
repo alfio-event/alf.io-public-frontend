f778d371d7b9a161eaebf26f4037ac8e1f7575bf
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-09-20 20:53:09 +0200

    add translate

--- src/app/poll/poll.module.ts
+++ src/app/poll/poll.module.ts
@@ -5,11 +5,13 @@ import { PollRoutingModule } from './poll-routing.module';
 import { PollComponent } from './poll.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DisplayPollComponent } from './display-poll/display-poll.component';
+import { TranslateModule } from '@ngx-translate/core'
 
 
 @NgModule({
   declarations: [PollComponent, DisplayPollComponent],
   imports: [
+    TranslateModule.forChild(),
     CommonModule,
     PollRoutingModule,
     FormsModule,
