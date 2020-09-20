d356d683321344e4ff2a792152bf1e224dfdc9b2
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-09-20 15:10:48 +0200

    fetch polls

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { EventService } from '../shared/event.service';
 import { PollService } from './shared/poll.service';
-import { from } from 'rxjs';
+import { Poll } from './model/poll';
 
 @Component({
   selector: 'app-live-poll',
@@ -13,6 +13,7 @@ import { from } from 'rxjs';
 export class PollComponent implements OnInit {
 
   pinForm: FormGroup;
+  polls: Poll[];
 
   constructor(private route: ActivatedRoute, private eventService: EventService, private pollService: PollService, private fb: FormBuilder) { }
 
@@ -29,8 +30,10 @@ export class PollComponent implements OnInit {
 
   confirmPin() {
     this.pollService.getAllPolls(this.route.snapshot.params['eventShortName'], this.pinForm.value.pin).subscribe(res => {
-      console.log(res);
-    })
+      if (res.success) {
+        this.polls = res.value;
+      }
+    });
   }
 
 }
