0de87af8c5f6a3b8205b101c8aecacb0c9108a27
Celestino Bellone <celestino@swicket.io>
2020-10-04 12:24:07 +0200

    add title, update layout

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../shared/event.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Event } from '../model/event';
+import { I18nService } from '../shared/i18n.service';
 
 @Component({
   selector: 'app-live-poll',
@@ -14,16 +15,17 @@ export class PollComponent implements OnInit {
   event: Event
 
   constructor(
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private eventService: EventService,
-    public translate: TranslateService) { }
+    public translate: TranslateService,
+    public i18nService: I18nService) { }
 
   ngOnInit(): void {
-    
+
     this.route.params.subscribe(params => {
       const eventShortName = params['eventShortName'];
       this.eventService.getEvent(eventShortName).subscribe(ev => {
-        //this.i18nService.setPageTitle('reservation-page-not-found.header.title', ev.displayName);
+        this.i18nService.setPageTitle('poll.page.title', ev.displayName);
         this.event = ev;
       });
     });
