1f0aec5cabe47c378b710d151b5cadbf5c43d762
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-12-29 13:54:41 +0100

    display all subscriptions

--- src/app/subscription-list-all/subscription-list-all.component.ts
+++ src/app/subscription-list-all/subscription-list-all.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { BasicSubscriptionInfo } from '../model/subscription';
+import { AnalyticsService } from '../shared/analytics.service';
+import { I18nService } from '../shared/i18n.service';
+import { InfoService } from '../shared/info.service';
+import { SubscriptionService } from '../shared/subscription.service';
+import { zip } from 'rxjs';
+import { Language } from '../model/event';
 
 @Component({
   selector: 'app-subscription-list-all',
@@ -7,10 +16,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionListAllComponent implements OnInit {
 
-  constructor() { }
+
+  subscriptions: BasicSubscriptionInfo[];
+  languages: Language[];
+
+  constructor(private subscriptionService: SubscriptionService,
+    private i18nService: I18nService,
+    private router: Router,
+    public translate: TranslateService,
+    private info: InfoService,
+    private analytics: AnalyticsService) { }
 
   ngOnInit(): void {
-    
+    zip(this.subscriptionService.getSubscriptions(), this.info.getInfo()).subscribe(([res, info]) => {
+      if (res.length === 1) {
+        this.router.navigate(['/subscription', res[0].id], {replaceUrl: true});
+      } else {
+        this.subscriptions = res;
+        this.analytics.pageView(info.analyticsConfiguration);
+      }
+    });
+
+    this.i18nService.getAvailableLanguages().subscribe(res => {
+      this.languages = res;
+    });
+
+    this.i18nService.setPageTitle('subscription.header.title', '');
   }
 
 }
