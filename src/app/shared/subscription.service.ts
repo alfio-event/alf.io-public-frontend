efd0ab4536d639bd79a9ada105899887d5f3e245
Sylvain Jermini <sylvain.jermini@syjer.com>
2021-01-13 16:43:31 +0100

    add cache for subscription too

--- src/app/shared/subscription.service.ts
+++ src/app/shared/subscription.service.ts
@@ -3,11 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BasicSubscriptionInfo, SubscriptionInfo } from '../model/subscription';
 import { ValidatedResponse } from '../model/validated-response';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
+
+
+  private subscriptionCache: {[key: string]: Observable<SubscriptionInfo>} = {};
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +20,13 @@ export class SubscriptionService {
   }
 
   getSubscriptionById(id: string): Observable<SubscriptionInfo> {
-    return this.http.get<SubscriptionInfo>(`/api/v2/public/subscription/${id}`);
+    if (!this.subscriptionCache[id]) {
+      this.subscriptionCache[id] = this.http.get<SubscriptionInfo>(`/api/v2/public/subscription/${id}`).pipe(shareReplay(1));
+      setTimeout(() => {
+        delete this.subscriptionCache[id];
+      }, 60000 * 20); // clean up cache after 20 minutes
+    }
+    return this.subscriptionCache[id];
   }
 
   reserve(id: string): Observable<ValidatedResponse<string>> {
