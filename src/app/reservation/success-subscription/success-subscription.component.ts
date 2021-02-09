c8bce3cc8e8e5cd7f20b1d4a0c011f37ad1e466b
Sylvain Jermini <sylvain.jermini@syjer.com>
2021-02-09 17:37:26 +0100

    add some stub for success subscription

--- src/app/reservation/success-subscription/success-subscription.component.ts
+++ src/app/reservation/success-subscription/success-subscription.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { zip } from 'rxjs';
+import { PurchaseContext } from 'src/app/model/purchase-context';
+import { ReservationInfo } from 'src/app/model/reservation-info';
+import { AnalyticsService } from 'src/app/shared/analytics.service';
+import { I18nService } from 'src/app/shared/i18n.service';
+import { PurchaseContextService, PurchaseContextType } from 'src/app/shared/purchase-context.service';
+import { ReservationService } from 'src/app/shared/reservation.service';
 
 @Component({
   selector: 'app-success-subscription',
@@ -7,9 +15,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessSubscriptionComponent implements OnInit {
 
-  constructor() { }
+  private publicIdentifier: string;
+  private reservationId: string;
+  private purchaseContextType: PurchaseContextType;
+  purchaseContext: PurchaseContext;
+  reservationInfo: ReservationInfo;
+
+  constructor(
+    private route: ActivatedRoute,
+    private purchaseContextService: PurchaseContextService,
+    private i18nService: I18nService,
+    private analytics: AnalyticsService,
+    private reservationService: ReservationService
+    ) { }
 
   ngOnInit(): void {
+    zip(this.route.data, this.route.params).subscribe(([data, params]) => {
+
+      this.publicIdentifier = params[data.publicIdentifierParameter];
+      this.reservationId = params['reservationId'];
+      this.purchaseContextType = data.type;
+
+      this.purchaseContextService.getContext(this.purchaseContextType, this.publicIdentifier).subscribe(ev => {
+        this.purchaseContext = ev;
+
+        this.i18nService.setPageTitle('reservation-page.header.title', ev.displayName);
+
+        this.loadReservation();
+
+        this.analytics.pageView(ev.analyticsConfiguration);
+      });
+    });
+  }
+
+  private loadReservation() {
+    this.reservationService.getReservationInfo(this.reservationId).subscribe(resInfo => {
+      this.reservationInfo = resInfo;
+    });
   }
 
 }
