713d91a55c4a03cdb31c046000b399f1f3baea76
Sylvain Jermini <sylvain.jermini@syjer.com>
2021-03-26 17:53:13 +0100

    cleanup language related css

--- src/app/shared/language-selector/language-selector.component.ts
+++ src/app/shared/language-selector/language-selector.component.ts
@@ -5,8 +5,7 @@ import { I18nService } from '../../shared/i18n.service';
 
 @Component({
   selector: 'app-language-selector',
-  templateUrl: './language-selector.component.html',
-  styleUrls: ['./language-selector.component.scss']
+  templateUrl: './language-selector.component.html'
 })
 export class LanguageSelectorComponent implements OnInit {
 
